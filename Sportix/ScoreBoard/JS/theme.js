2cb9f9b9288db52f38f836a518837736715bbb9b
Shahadat Hossain Sakib <152428049+IamShahadat00@users.noreply.github.com>
2025-03-02 22:02:24 +0600

    Delete Sportix directory

--- Sportix/ScoreBoard/JS/theme.js
+++
@@ -1,6 +0,0 @@
-function theme() {
-    const element = document.body;
-    element.classList.toggle("dark-mode");
-    const btn = document.getElementById('theme');
-    btn.innerHTML = element.classList.contains("dark-mode") ? "☀️ Light Mode" : "🌙 Dark Mode";
-}
